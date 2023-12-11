ff0edf37fef57f8c1dbdd74a549bc2ee9bbe9503
ashish9599 <124872980+ashish9599@users.noreply.github.com>
2023-12-12 01:11:33 +0530

    2 commit

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -1,7 +1,14 @@
+import { Navigate } from "react-router-dom";
+import { useAuth } from "../hook/authHook";
 import Allbook from "../pages/books/Allbook";
 import Sidebar from "./Sidebar";
 
 const Home = () => {
+  const { user } = useAuth();
+
+  if (!user) {
+    return <Navigate to="/login" />;
+  }
   return (
     <div className="Home" style={{ display: "flex" }}>
       <Sidebar />
